eda1ad98a6f0a25076f3f7bd14a97360a2cb3b56
twistedturtle <hindredkin@gmail.com>
2025-02-19 17:40:08 +0000

    add signal handler to prompt

--- src/inquirer/prompt.py
+++ src/inquirer/prompt.py
@@ -3,6 +3,11 @@ from inquirer.render.console import ConsoleRender
 
 import signal
 
+def signal_handler(sig, frame):
+    sys.exit(1)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 def prompt(questions, render=None, answers=None, theme=themes.Default(), raise_keyboard_interrupt=False, raise_sigint=True, int_msg=True):
     render = render or ConsoleRender(theme=theme)
     answers = answers or {}
