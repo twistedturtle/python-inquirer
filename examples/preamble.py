849878bc7e56c9cce42f44f449a1433913d8c857
twistedturtle <hindredkin@gmail.com>
2025-02-19 17:41:36 +0000

    add example for preamble

---
+++ examples/preamble.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from pprint import pprint
+
+
+# sys.path.append(os.path.realpath("."))
+import inquirer  # noqa
+
+
+questions = [
+    inquirer.List(
+        "size",
+        preamble="This will be shown before the question\n\n",
+        message="What size do you need?",
+        choices=["Jumbo", "Large", "Standard", "Medium", "Small", "Micro"],
+    ),
+]
+
+answers = inquirer.prompt(questions)
+
+pprint(answers)
