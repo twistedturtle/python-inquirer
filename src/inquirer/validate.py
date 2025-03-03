54628a0995684ab2b57fdc030fd8fbf6bad7c97c
twistedturtle <hindredkin@gmail.com>
2025-03-03 15:18:44 +0000

    add is_float validation and custom error messages for other validation functions

--- src/inquirer/validate.py
+++ src/inquirer/validate.py
@@ -1,38 +1,41 @@
-
-
 import hashlib
 
+from inquirer.errors import ValidationError
 
-
-def is_integer(answers, current):
-    if current.isdecimal():
-        return True
-    return False
 
 def is_decimal(answers, current):
     if current.isdecimal():
         return True
-    return False
+    raise ValidationError("", reason=f'"{current}" is not a base 10 integer')
+
 
 def is_digit(answers, current):
     if current.isdigit():
         return True
-    return False
+    raise ValidationError("", reason=f'"{current}" is not a number')
+
+
+def is_float(answers, current):
+    try:
+        float(current)
+        return True
+    except ValueError:
+        raise ValidationError("", reason=f'"{current}" is not a float')
 
 
 def is_alpha(answers, current):
     if current.isalpha():
         return True
-    return False
+    raise ValidationError("", reason=f'"{current}" is not a alphabetical')
+
 
 def is_alphanumeric(answers, current):
     if current.isalnum():
         return True
-    return False
+    raise ValidationError("", reason=f'"{current}" is not alphanumeric')
 
 
 def is_hashlib(answers, current):
     if current in hashlib.algorithms_guaranteed:
         return True
-    return False
-
+    raise ValidationError("", reason=f'"{current}" is not a hash in hashlib')
