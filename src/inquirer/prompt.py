08604022ed9b7b8ba11ab3d71fa6e03d6cf218c5
twistedturtle <hindredkin@gmail.com>
2025-03-13 23:54:01 +0000

    merge confirm_prompt into prompt, allow passing a single question object

--- src/inquirer/prompt.py
+++ src/inquirer/prompt.py
@@ -1,78 +1,64 @@
-import inquirer.themes as themes
-from inquirer.render.console import ConsoleRender
-import inquirer
-
-
+import inspect
 import signal
 import sys
-import inspect
+from typing import Optional
+from typing import Union
+
+import inquirer
+from inquirer.questions import Question
+from inquirer.render.console import ConsoleRender as CR
+from inquirer.themes import Default
 
 
+# This is in case raise_sigint is True but the
+# signal isn't handled in the parent software
+# parent handler will override
 def signal_handler(sig, frame):
     sys.exit(1)
+
 
 signal.signal(signal.SIGINT, signal_handler)
 
 
 def prompt(
-    questions,
-    render=ConsoleRender,
-    answers={},
-    theme=themes.Default(),
-    raise_keyboard_interrupt=False,
-    raise_sigint=False,
-    int_msg=True,
+    questions: Optional[Union[Question, list[Question]]] = None,
+    render: Union[CR, type[CR]] = CR,
+    answers: Optional[dict] = None,
+    theme: Union[Default, type[Default]] = Default(),
+    raise_keyboard_interrupt: bool = False,
+    raise_sigint: Optional[bool] = None,
+    int_msg: bool = True,
+    message: str = "Do you want to continue?",
+    **kwargs
 ):
+    answers = answers or {}
+
     if inspect.isclass(theme):
         theme = theme()
 
     if inspect.isclass(render):
         render = render(theme=theme)
+
+    single = False
+    if questions is None:
+        single = True
+        questions = [inquirer.Confirm("confirm", message=message, **kwargs)]
+
+    if not isinstance(questions, list):
+        single = True
+        questions = [questions]
 
     try:
         for question in questions:
             answers[question.name] = render.render(question, answers)
+        if single:
+            return answers[question.name]
         return answers
     except KeyboardInterrupt:
         if int_msg:
             print("\033[K" + theme.Question.int_msg)
-        if raise_keyboard_interrupt:
-            raise
         if raise_sigint:
             signal.raise_signal(signal.SIGINT)
-
-
-
-def confirm_prompt(
-    msg="Do you want to continue?",
-    default=True,
-    render=ConsoleRender,
-    answers={},
-    theme=themes.Default(),
-    raise_keyboard_interrupt=False,
-    raise_sigint=False,
-    int_msg=True,
-):
-
-    if inspect.isclass(theme):
-        theme = theme()
-
-    if inspect.isclass(render):
-        render = render(theme=theme)
-
-    question = inquirer.Confirm("confirm", message=msg, default=default)
-
-    try:
-        # for question in questions:
-        answers[question.name] = render.render(question, answers)
-        if not answers == None and answers["confirm"]:
-            return True
-        return False
-
-    except KeyboardInterrupt:
-        if int_msg:
-            print("\033[K" + theme.Question.int_msg)
         if raise_keyboard_interrupt:
             raise
-        if raise_sigint:
-            signal.raise_signal(signal.SIGINT)
+
